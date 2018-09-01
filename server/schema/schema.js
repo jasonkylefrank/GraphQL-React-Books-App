0fcc12ba1019731bf3208dd21b18d103e21da3c0
Jason K. Frank <jasonkylefrank@gmail.com>
2018-09-01 15:04:13 -0400

    Added non-null requirements to the mutations (through lesson #21)

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -9,7 +9,9 @@ const {
     GraphQLSchema,
     GraphQLID,
     GraphQLInt,
-    GraphQLList } = graphql;
+    GraphQLList,
+    GraphQLNonNull 
+} = graphql;
 
 
 // // Dummy data
@@ -138,8 +140,8 @@ const Mutation = new GraphQLObjectType({
         addAuthor: {
             type: AuthorType,
             args: { 
-                name: { type: GraphQLString },
-                age: { type: GraphQLInt }
+                name: { type: new GraphQLNonNull(GraphQLString) },
+                age: { type: new GraphQLNonNull(GraphQLInt) }
              },
              resolve(parent, args) {
                  // Create an instance of the mongoose model
@@ -156,9 +158,9 @@ const Mutation = new GraphQLObjectType({
         addBook: {
             type: BookType,
             args: {
-                name: { type: GraphQLString },
-                genre: { type: GraphQLString },
-                authorId: { type: GraphQLID }
+                name: { type: new GraphQLNonNull(GraphQLString) },
+                genre: { type: new GraphQLNonNull(GraphQLString) },
+                authorId: { type: new GraphQLNonNull(GraphQLID) }
             },
             resolve(parent, args) {
                 // Create an instance of the mongoose model

e2f8efd1cc69bc72c2c7d3a757506c8341234cdc
Jason K. Frank <jasonkylefrank@gmail.com>
2018-09-01 12:11:25 -0400

    Added more root queries for "authors" and "books" (through lesson #15)

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,9 +1,9 @@
 const graphql = require('graphql');
 const _ = require('lodash');
 
-const { 
-    GraphQLObjectType, 
-    GraphQLString, 
+const {
+    GraphQLObjectType,
+    GraphQLString,
     GraphQLSchema,
     GraphQLID,
     GraphQLInt,
@@ -13,16 +13,17 @@ const {
 
 // dummy data
 let books = [
-    { name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1'},
-    { name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2'},
-    { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3'},
-    { name: 'The Fake Book', genre: 'Sci-Fi', id: '0', authorId: '3'}
+    { name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1' },
+    { name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2' },
+    { name: 'Some Book Name', genre: 'Sci-Fi', id: '4', authorId: '2' },
+    { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3' },
+    { name: 'The Fake Book', genre: 'Sci-Fi', id: '0', authorId: '3' }
 ];
 
 let authors = [
-    { name: 'Patrick Rothfuss', age: 44, id: '1', bookIds: ['1']},
-    { name: 'Brandon Sanderson', age: 42, id: '2', bookIds: ['2']},
-    { name: 'Terry Pratchett', age: 66, id: '3', bookIds: ['0', '3']}
+    { name: 'Patrick Rothfuss', age: 44, id: '1', bookIds: ['1'] },
+    { name: 'Brandon Sanderson', age: 42, id: '2', bookIds: ['2'] },
+    { name: 'Terry Pratchett', age: 66, id: '3', bookIds: ['0', '3'] }
 ];
 
 
@@ -37,26 +38,38 @@ const BookType = new GraphQLObjectType({
             resolve(parent, args) {
                 return _.find(authors, { id: parent.authorId });
             }
-         }
+        }
     })
 });
 
 const AuthorType = new GraphQLObjectType({
     name: 'Author',
+    // We need to use a FUNCTION for the fields value so that it will be
+    //  executed after the entire file is parsed, which will enable GraphQL
+    //  to know about all of our custom types, such as AuthorType, BookType,
+    //  etc.  Otherwise it would error-out on some of those types as it
+    //  reads the file top-to-bottom and finds a custom type that is defined
+    //  farther down the file.
+    //  See: https://youtu.be/ed8SzALpx1Q?t=4969
     fields: () => ({
         id: { type: GraphQLID },
         name: { type: GraphQLString },
-        age: { type:  GraphQLInt },
+        age: { type: GraphQLInt },
         books: {
+            // Note this type, which enables us to return an array
+            // (see: https://jaketrent.com/post/return-array-graphql/ )
             type: new GraphQLList(BookType),
-            resolve(parent, args) {                
-                const authorBooks = books.filter((book) => {
-                    const match = parent.bookIds.find((authorBookId) => (
-                        authorBookId === book.id
-                    ));             
-                    return match !== undefined;
-                });
-                return authorBooks;
+            resolve(parent, args) {
+                // const authorBooks = books.filter((book) => {
+                //     const match = parent.bookIds.find((authorBookId) => (
+                //         authorBookId === book.id
+                //     ));             
+                //     return match !== undefined;
+                // });
+                // return authorBooks;
+
+                // Or if we use underscore, we could do it like this:
+                return _.filter(books, { authorId: parent.id });
             }
         }
     })
@@ -67,22 +80,34 @@ const RootQuery = new GraphQLObjectType({
     // each of these "fields" will be root queries
     //  (or entry points)
     fields: {
-        book: { 
+        book: {
             type: BookType,
             args: { id: { type: GraphQLID } },
             resolve(parent, args) {
                 // Code to get data from db or other source
-                console.log(typeof(args.id));
-                return _.find(books, { id: args.id});
+                console.log(typeof (args.id));
+                return _.find(books, { id: args.id });
             }
-         },
-         author: {
-             type: AuthorType,
-             args: { id: { type: GraphQLID } },
-             resolve(parent, args) {
-                 return _.find(authors, { id: args.id });
-             }
-         }
+        },
+        author: {
+            type: AuthorType,
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args) {
+                return _.find(authors, { id: args.id });
+            }
+        },
+        books: {
+            type: new GraphQLList(BookType),
+            resolve(parent, args) {
+                return books;
+            }
+        },
+        authors: {
+            type: new GraphQLList(AuthorType),
+            resolve(parent, args) {
+                return authors;
+            }
+        }
     }
 });
 

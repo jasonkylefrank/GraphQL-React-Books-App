2f22b36a83c2086d1ad0bdfef25fe70505633845
Jason K. Frank <jasonkylefrank@gmail.com>
2018-09-01 15:54:02 -0400

    Added a starting React app from create-react-app

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+// components
+import BookList from './components/BookList';
+
+class App extends Component {
+  render() {
+    return (
+      <div id="main">
+        <h1>Jason's reading list</h1>
+        <BookList/>
+
+
+      </div>
+    );
+  }
+}
+
+export default App;

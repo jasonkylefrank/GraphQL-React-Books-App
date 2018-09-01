982f4d88c6e8337721ba3081040096628ccc1dc7
Jason K. Frank <jasonkylefrank@gmail.com>
2018-09-01 16:14:48 -0400

    Added a basic Apollo setup but did not add queries to it yet (through lesson #25)

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+
 // components
 import BookList from './components/BookList';
 
+
+// ApolloClient setup
+const client = new ApolloClient({
+  // Set the endpoint that we'll make queries to
+  uri: 'http://localhost:4000/graphql'
+});
+
 class App extends Component {
+  // The ApolloProvider will inject data from the endpoint into our components
+  //  because it is wrapping everything in the app.
   render() {
     return (
-      <div id="main">
-        <h1>Jason's reading list</h1>
-        <BookList/>
+      <ApolloProvider client={client}>
+        <div id="main">
+          <h1>Jason's reading list</h1>
+          <BookList />
 
-
-      </div>
+        </div>
+      </ApolloProvider>
     );
   }
 }

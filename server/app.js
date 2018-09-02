769508ffb1469ef7ea57abf2a43b93352066a12c
Jason K. Frank <jasonkylefrank@gmail.com>
2018-09-02 14:22:18 -0400

    Fixed a problem locally that was causing my DB connection to fail...
    My ".env" file (which stores DB credentials and such) was not in the "server" directory where it needed to be.  It needs to be in the same directory as the app.js file which pulls in the dotenv module and runs its config() function.  At that time that module will look in the same directory for the ".env" file.  Since it was not there it failed to pull my DB credential values in.
    
    Although the ".env" file is not in source control, I made better notes in the app.js file to help aleviate this confusion.

--- server/app.js
+++ server/app.js
@@ -4,7 +4,8 @@ const schema = require('./schema/schema');
 const mongoose = require('mongoose');
 const cors = require('cors');
 // Configure the dotenv module so we can access our DB credentials or
-//  other values from environment variables.
+//  other values from environment variables. It will look for a ".env"
+//  file in the same directory to get its config values from.
 require('dotenv').config();
 
 const app = express();
@@ -23,11 +24,15 @@ const dbName = process.env.DB_NAME;
 const connectionString = 
     `mongodb://${dbUser}:${dbPassword}@ds141932.mlab.com:41932/${dbName}`;
 
-mongoose.connect(connectionString);
+mongoose.connect(connectionString, { useNewUrlParser: true });
 // Add an event listener to fire a function once the connection is open
 mongoose.connection.once('open', () => {
     console.log('Connected to database');
 });
+mongoose.connection.on(
+    'error', 
+    console.error.bind(console, 'Connection error: ')
+);
 
 // Add the GraphQL middleware so Express can handle queries to the
 //  "/graphql" endpoint
@@ -38,6 +43,7 @@ app.use('/graphql', graphqlHTTP({
 
 
 app.listen(port, () => { 
-    console.log(`Now listening for requests on port ${port}...`) 
+    console.log(`Now listening for requests on port ${port}...`);
+    console.log(''); 
 });
 
